c5ff689298e0ce4bb7f4a27aa53b6aa657fa9c54
Erick Gusson <renongusson@gmail.com>
2024-01-28 12:21:33 -0300

    .
    Exercicio 4

--- WindowsFormsLista/Ex4.Designer.cs
+++ WindowsFormsLista/Ex4.Designer.cs
@@ -44,9 +44,10 @@
             // cbxLetras
             // 
             this.cbxLetras.AutoSize = true;
+            this.cbxLetras.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxLetras.Location = new System.Drawing.Point(29, 190);
             this.cbxLetras.Name = "cbxLetras";
-            this.cbxLetras.Size = new System.Drawing.Size(55, 17);
+            this.cbxLetras.Size = new System.Drawing.Size(67, 21);
             this.cbxLetras.TabIndex = 0;
             this.cbxLetras.Text = "Letras";
             this.cbxLetras.UseVisualStyleBackColor = true;
@@ -54,9 +55,10 @@
             // cbxNumeros
             // 
             this.cbxNumeros.AutoSize = true;
+            this.cbxNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxNumeros.Location = new System.Drawing.Point(29, 213);
             this.cbxNumeros.Name = "cbxNumeros";
-            this.cbxNumeros.Size = new System.Drawing.Size(68, 17);
+            this.cbxNumeros.Size = new System.Drawing.Size(84, 21);
             this.cbxNumeros.TabIndex = 1;
             this.cbxNumeros.Text = "Números";
             this.cbxNumeros.UseVisualStyleBackColor = true;
@@ -64,9 +66,10 @@
             // cbxEspeciais
             // 
             this.cbxEspeciais.AutoSize = true;
+            this.cbxEspeciais.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxEspeciais.Location = new System.Drawing.Point(29, 236);
             this.cbxEspeciais.Name = "cbxEspeciais";
-            this.cbxEspeciais.Size = new System.Drawing.Size(125, 17);
+            this.cbxEspeciais.Size = new System.Drawing.Size(160, 21);
             this.cbxEspeciais.TabIndex = 2;
             this.cbxEspeciais.Text = "Caracteres Especiais";
             this.cbxEspeciais.UseVisualStyleBackColor = true;
@@ -74,15 +77,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(26, 164);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.Size = new System.Drawing.Size(298, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Deseja Incluir quais elementos na sua senha?";
             // 
             // txbResultado
             // 
-            this.txbResultado.Location = new System.Drawing.Point(29, 348);
+            this.txbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbResultado.Location = new System.Drawing.Point(29, 352);
             this.txbResultado.Multiline = true;
             this.txbResultado.Name = "txbResultado";
             this.txbResultado.Size = new System.Drawing.Size(198, 57);
@@ -91,17 +96,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(26, 332);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(93, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "RESULTADO";
             // 
             // nudTamanho
             // 
+            this.nudTamanho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nudTamanho.Location = new System.Drawing.Point(29, 127);
             this.nudTamanho.Name = "nudTamanho";
-            this.nudTamanho.Size = new System.Drawing.Size(120, 20);
+            this.nudTamanho.Size = new System.Drawing.Size(120, 23);
             this.nudTamanho.TabIndex = 6;
             this.nudTamanho.Value = new decimal(new int[] {
             1,
@@ -112,14 +119,16 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(26, 102);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(270, 13);
+            this.label3.Size = new System.Drawing.Size(363, 17);
             this.label3.TabIndex = 7;
             this.label3.Text = "Defina o tamanho da sua senha (número de caracteres)";
             // 
             // btnGerarSenha
             // 
+            this.btnGerarSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGerarSenha.Location = new System.Drawing.Point(29, 285);
             this.btnGerarSenha.Name = "btnGerarSenha";
             this.btnGerarSenha.Size = new System.Drawing.Size(120, 23);
@@ -132,6 +141,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.label4.Location = new System.Drawing.Point(25, 20);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(235, 24);
